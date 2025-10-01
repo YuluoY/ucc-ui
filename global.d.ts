7a8b6f879260e70c6bb6e7ea99cac41f569b1e58
huyongle <568055454@qq.com>
2025-10-02 02:12:28 +0800

    fix: 修复已知问题

--- global.d.ts
+++ global.d.ts
@@ -15,11 +15,11 @@
     UDropdownMenu: typeof import('ucc-ui')['UDropdownMenu'];
     UForm: typeof import('ucc-ui')['UForm'];
     UFormItem: typeof import('ucc-ui')['UFormItem'];
-    UIcon: typeof import('ucc-ui')['UIcon'];
     UInput: typeof import('ucc-ui')['UInput'];
     UInputNumber: typeof import('ucc-ui')['UInputNumber'];
     ULayout: typeof import('ucc-ui')['ULayout'];
     URegion: typeof import('ucc-ui')['URegion'];
+    UIcon: typeof import('ucc-ui')['UIcon'];
     UMenu: typeof import('ucc-ui')['UMenu'];
     UMenuItem: typeof import('ucc-ui')['UMenuItem'];
     USubMenu: typeof import('ucc-ui')['USubMenu'];
@@ -27,12 +27,12 @@
     UReadProgress: typeof import('ucc-ui')['UReadProgress'];
     USlider: typeof import('ucc-ui')['USlider'];
     UTable: typeof import('ucc-ui')['UTable'];
-    UTag: typeof import('ucc-ui')['UTag'];
     UText: typeof import('ucc-ui')['UText'];
+    UTag: typeof import('ucc-ui')['UTag'];
+    UTop: typeof import('ucc-ui')['UTop'];
     UTimeline: typeof import('ucc-ui')['UTimeline'];
     UTimelineItem: typeof import('ucc-ui')['UTimelineItem'];
     UTooltip: typeof import('ucc-ui')['UTooltip'];
-    UTop: typeof import('ucc-ui')['UTop'];
 }
   }
   declare module '@vue/runtime-core' {
